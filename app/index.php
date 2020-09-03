317803ca85e633039788a0bd762de3c7a9261cb3
Vitor Fernandes <31454363+vitoor17@users.noreply.github.com>
2020-09-03 14:06:49 -0300

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Devops inter assessment</title>
+    <meta name="description" content="Devops inter assessment">
+    <meta name="author" content="Rockcontent">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+  </head>
+
+  <body>
+    <h1>Rock Content</h1>
+    <h2>Stage team</h2>
+    <?php
+      // Silence is golden
+    ?>
+    <script src="js/scripts.js"></script>
+  </body>
+</html>
