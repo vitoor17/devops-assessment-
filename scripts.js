127056cb48a0ef6a5625a0e77bcabb89a713434b
Vitor Fernandes <31454363+vitoor17@users.noreply.github.com>
2020-09-03 10:07:06 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const array = [
+  { "name": "sample 1", "age": 25, "id": 123 },
+  { "name": "sample 2", "age": 26, "id": 124 },
+  { "name": "sample 3", "age": 27, "id": 125 },
+  { "name": "sample 4", "age": 28, "id": 126 },
+  { "name": "sample 4", "age": 29, "id": 127 }
+];
+console.table(array);
